b009e2edd125d6b20f27287ef40e87063fbd3a57
amoraga <amoraga@colmena.cl>
2020-11-11 09:40:59 -0300

    vectorize resample

--- ml_smote.py
+++ ml_smote.py
@@ -32,22 +32,31 @@ def as_np_array(fn):
 def values(x):
     return x.values
 
+
 class MLSMOTE:
     def __init__(self, *, backend='cuda', seed=123, **nn_args):
         if backend == 'cuda':
-            self.db = cudf
-            self.cb = cuml
+            self.dfb = cudf
+            self.arrayb = cupy
+            self.mlb = cuml
         elif backend == 'cpu':
-            self.db = pd
-            self.cb = sklearn
+            self.dfb = pd
+            self.arrayb = np
+            self.mlb = sklearn
         self.nn_args = nn_args
         self.seed = seed
 
     def nn_wrapper(self, X):
-        nn = self.cb.neighbors.NearestNeighbors(**self.nn_args)
+        nn = self.mlb.neighbors.NearestNeighbors(**self.nn_args)
         nn.fit(X)
         _, idxs = nn.kneighbors(X)
         return idxs
+
+    def nn_sum(self, y, all_nbs):
+        res = self.arrayb.zeros((len(all_nbs))
+        for i, nbs in enumerate(all_nbs):
+            res[i] = y[nbs].sum(axis=0)
+        return res
 
     def resample(self, X, y, n_samples):
         np.random.seed(self.seed)
@@ -57,10 +66,13 @@ class MLSMOTE:
         idxs = values(self.nn_wrapper(X_masked))
         
         sample_idx = np.random.choice(idxs[:, 0], n_samples)
-        nbs_idx = [np.random.choice(idxs[j, 1:]) for j in sampled_idx]
-        sum_nn = y.loc[idxs[sample_idx]].sum(axis=0)
-        y_res = sum_nn.mask(sum_nn > 2, 1, 0)
+        nbs_idx = [np.random.choice(idxs[j, 1:]) for j in sample_idx]
+        nn_sum = self.nn_sum(y_masked.values, idxs[sample_idx])
+        y_res = self.dfb.DataFrame(
+            self.arrayb.where(nn_sum >= 1, 1, 0),
+            columns=y.columns
+            )
         X_res = inbetween_sample(X_masked.loc[sample_idx],
                                 X_masked.loc[nbs_idx])
         
-        return self.db.concat([X, X_res], axis=0), self.db.concat([y, y_res], axis=0)
+        return self.dfb.concat([X, X_res], axis=0), self.dfb.concat([y, y_res], axis=0)
